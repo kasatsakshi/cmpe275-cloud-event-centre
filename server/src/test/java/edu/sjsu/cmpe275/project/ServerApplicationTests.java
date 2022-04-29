6aee5efb1f11de36e2efa8e6b093e2a0a7f5e371
Sudheendra Katikar <sudhu.katikar@gmail.com>
2022-04-28 18:03:14 -0700

    server structure

---
+++ server/src/test/java/edu/sjsu/cmpe275/project/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.sjsu.cmpe275.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
