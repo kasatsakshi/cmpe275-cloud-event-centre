2116d5c69d7acc3dc5f03351b6c7b0fd7e48b96b
sakshikasat <kasat.sakshi94@gmail.com>
2022-05-11 12:13:59 -0700

    fixes to form front end

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,87 +1,104 @@
 import React from 'react';
-import './Register.css';
+import FormLabel from '@mui/material/FormLabel';
+import TextField from '@mui/material/TextField';
 import Radio from '@mui/material/Radio';
 import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
-import FormControl from '@mui/material/FormControl';
-import FormLabel from '@mui/material/FormLabel';
-import TextField from '@mui/material/TextField';
+import './Register.css';
 
 function Register() {
   return (
-    <div class="container" id="container">
-      <div className='form-container register-container'>
-        <form action="#">
-          <h1>Register Details</h1>
-          <div className='register-radio'>
-            {/* <FormLabel className='register-label' id="account-type">Account Type</FormLabel> */}
-            <RadioGroup
-              row
-              aria-labelledby="account-type"
-              name="account-type-buttons"
-            >
-              <FormControlLabel value="person" control={<Radio />} label="Person" />
-              <FormControlLabel value="organization" control={<Radio />} label="Organization" />
-            </RadioGroup>
-          </div>
-          <TextField
-            required
-            id="name"
-            label="Required"
-            defaultValue='Name'
-            className='register-input-fields'
-          />
-          <div className='register-radio'>
-            <FormLabel>Gender</FormLabel>
-            <RadioGroup
-              row
-              aria-labelledby="demo-row-radio-buttons-group-label"
-              name="row-radio-buttons-group"
-            >
-              <FormControlLabel value="female" control={<Radio />} label="Female" />
-              <FormControlLabel value="male" control={<Radio />} label="Male" />
-              <FormControlLabel value="other" control={<Radio />} label="Other" />
-            </RadioGroup>
-          </div>
-          <TextField
-            required
-            id="name"
-            placeholder='Screen Name'
-            className='register-input-fields'
-          />
-          <TextField
-            required
-            id="name"
-            placeholder='Description'
-            className='register-input-fields'
-          />
-          <TextField
-            required
-            id="name"
-            placeholder='Address1'
-            className='register-input-fields'
-          />
-          <TextField
-            required
-            id="name"
-            placeholder='City'
-            className='register-input-fields'
-          />
-          <TextField
-            required
-            id="name"
-            placeholder='State'
-            className='register-input-fields'
-          />
-          <TextField
-            required
-            id="name"
-            placeholder='Zip'
-            className='register-input-fields'
-          />
-          <button className='register-button'>Register</button>
-        </form>
-      </div>
+
+    <div className='register-container'>
+      <form action="#">
+        <h1>Register Details</h1>
+        <div className='register-radio'>
+          {/* <FormLabel className='register-label' id="account-type">Account Type</FormLabel> */}
+          <RadioGroup
+            row
+            aria-labelledby="account-type"
+            name="account-type-buttons"
+          >
+            <FormControlLabel value="person" control={<Radio />} label="Person" />
+            <FormControlLabel value="organization" control={<Radio />} label="Organization" />
+          </RadioGroup>
+        </div>
+        <TextField
+          required
+          id="name"
+          placeholder='Enter your full name'
+          className='register-input-fields'
+          style={{ paddingBottom: 20 }}
+        />
+        <TextField
+          required
+          id="email"
+          placeholder='email'
+          className='register-input-fields'
+          style={{ paddingBottom: 20 }}
+        />
+        <TextField
+          required
+          id="password"
+          placeholder='password'
+          className='register-input-fields'
+          style={{ paddingBottom: 20 }}
+        />
+        <div className='register-radio'>
+          <FormLabel>Gender</FormLabel>
+          <RadioGroup
+            row
+            aria-labelledby="demo-row-radio-buttons-group-label"
+            name="row-radio-buttons-group"
+          >
+            <FormControlLabel value="female" control={<Radio />} label="Female" />
+            <FormControlLabel value="male" control={<Radio />} label="Male" />
+            <FormControlLabel value="other" control={<Radio />} label="Other" />
+          </RadioGroup>
+        </div>
+        <TextField
+          required
+          id="name"
+          placeholder='Screen Name'
+          className='register-input-fields'
+          style={{ paddingBottom: 20 }}
+        />
+        <TextField
+          required
+          id="name"
+          placeholder='Description'
+          className='register-input-fields'
+          style={{ paddingBottom: 20 }}
+        />
+        <TextField
+          required
+          id="name"
+          placeholder='Street (optional)'
+          className='register-input-fields'
+          style={{ paddingBottom: 20 }}
+        />
+        <TextField
+          required
+          id="name"
+          placeholder='City'
+          className='register-input-fields'
+          style={{ paddingBottom: 20 }}
+        />
+        <TextField
+          required
+          id="name"
+          placeholder='State'
+          className='register-input-fields'
+          style={{ paddingBottom: 20 }}
+        />
+        <TextField
+          required
+          id="name"
+          placeholder='Zip'
+          className='register-input-fields'
+        />
+        <button className='register-button'>Register</button>
+      </form>
     </div>
   )
 }
