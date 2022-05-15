e4284eef7a2535ffe753828b87e480d4ab0c88ff
sakshikasat <kasat.sakshi94@gmail.com>
2022-05-14 20:00:44 -0700

    Add forum buttons

--- client/src/components/EventPage.js
+++ client/src/components/EventPage.js
@@ -6,9 +6,21 @@ import './EventPage.css';
 import { Link } from 'react-router-dom';
 import EventNavbar from "./EventNavbar";
 import { useSelector } from "react-redux";
-import { Navigation } from 'react-minimal-side-navigation';
-import 'react-minimal-side-navigation/lib/ReactMinimalSideNavigation.css';
-import Sidebar from "./Sidebar";
+import styled from "styled-components";
+
+const Button = styled.button`
+  width: 200px;
+  border: none;
+  padding: 15px 20px;
+  background-color: black;
+  color: white;
+  cursor: pointer;
+  margin-bottom: 10px;
+  &:disabled {
+    background-color: grey;
+    cursor: not-allowed;
+  }
+`;
 
 function EventPage() {
   const user = useSelector((state) => state.user.currentUser);
@@ -48,28 +60,25 @@ function EventPage() {
   return (
     <div>
       <EventNavbar />
-      <div className="event-page-container">
-        <div className="event-sidebar">
-          <Sidebar />
+      <div className="event-container">
+        <h1 className="event-title">{event.title}</h1>
+        <div className="event-description">{event.description}</div>
+        {/* <div className="event-owner event-text">by: {event.creator.screenName}</div> */}
+        <div className="event-date event-text">
+          <div className="event-start event-text">Start Time: {event.startTime}</div>
+          <div className="event-text">End time: {event.endTime}</div>
         </div>
-        <div className="event-container">
-          <h1 className="event-title">{event.title}</h1>
-          <div className="event-description">{event.description}</div>
-          {/* <div className="event-owner event-text">by: {event.creator.screenName}</div> */}
-          <div className="event-date event-text">
-            <div className="event-start event-text">Start Time: {event.startTime}</div>
-            <div className="event-text">End time: {event.endTime}</div>
-          </div>
 
 
-          <div className="event-fee event-text">Fee: {event.fee}$</div>
-          <div className="event-date">
-            <div className="event-start event-text">Min participant: {event.minimumParticipants}</div>
-            <div className="event-text">Max participant: {event.maximumParticipants}</div>
-          </div>
-          {/* <div className="event-participants event-text">Current number of participants: {event.participants.length}</div> */}
-          <button onClick={() => signupevents()}>Sign Up</button>
+        <div className="event-fee event-text">Fee: {event.fee}$</div>
+        <div className="event-date">
+          <div className="event-start event-text">Min participant: {event.minimumParticipants}</div>
+          <div className="event-text">Max participant: {event.maximumParticipants}</div>
         </div>
+        {/* <div className="event-participants event-text">Current number of participants: {event.participants.length}</div> */}
+        <Button disabled={event.status !== "REGISTRATION_OPEN"} className="event-button" onClick={() => signupevents()}>Sign Up</Button>
+        <Button className="event-button" onClick={() => signupevents()}>Signup Forum</Button>
+        <Button className="event-button" onClick={() => signupevents()}>Participant Forum</Button>
       </div>
     </div>
   );
