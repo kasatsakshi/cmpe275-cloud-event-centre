31e905f6500fa678af4edc8f11dd8296a6615c26
sakshikasat <kasat.sakshi94@gmail.com>
2022-05-25 16:14:28 -0700

    System reports dashboard ready

--- client/src/components/SystemReport.js
+++ client/src/components/SystemReport.js
@@ -7,7 +7,9 @@ import { PieChart, Pie, Sector, Cell, Tooltip, ResponsiveContainer } from 'recha
 
 function SystemReport() {
   const [paidData, setPaidData] = React.useState([]);
+  const [systemData, setSystemData] = React.useState([]);
   const [mimicDate, setMimicDate] = React.useState(null);
+  const COLORS = ['#e75d45', '#AEB2B0'];
 
   useEffect(() => {
     axios
@@ -31,8 +33,8 @@ function SystemReport() {
               { name: 'unpaid Events', value: 100 - res.data.percentPaidEvents },
               { name: 'Paid Events', value: res.data.percentPaidEvents }
             ];
-
             setPaidData(paidData);
+            setSystemData(res.data);
           }).catch((err) => {
             console.log(err);
           });
@@ -47,20 +49,34 @@ function SystemReport() {
       <EventNavbar />
       <div className='report-container'>
         <h1>System Report</h1>
-        <ResponsiveContainer width="100%" height={400}>
-          <PieChart width={400} height={400}>
-            <Pie
-              dataKey="value"
-              isAnimationActive={false}
-              data={paidData}
-              cx="50%"
-              cy="50%"
-              outerRadius={80}
-              fill="#8884d8"
-              label />
-            <Tooltip />
-          </PieChart>
-        </ResponsiveContainer>
+        <div className='events-created'>
+          <h6 className='report-heading'>Total events created: {systemData.createdEvents}</h6>
+          <h6>Percent of paid events: {systemData.percentPaidEvents}%</h6>
+          <ResponsiveContainer width="100%" height={280}>
+            <PieChart width={400} height={400}>
+              <Pie
+                dataKey="value"
+                isAnimationActive={false}
+                data={paidData}
+                cx="50%"
+                cy="50%"
+                outerRadius={80}
+                fill="#e75d45"
+                label="value">
+                {paidData.map((entry, index) => (
+                  <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                ))}
+              </Pie>
+              <Tooltip />
+            </PieChart>
+          </ResponsiveContainer>
+        </div>
+        <div>
+          <h6>Number of Cancelled events: {systemData.canceledEvent}</h6>
+          <h6>Total number of participation request / minimum participant required: {systemData.requestsPerMinParticipants}</h6>
+          <h6 className='report-heading'>Number of Finished Events: {systemData.finishedEvents}</h6>
+          <h6>Average participants: {systemData.avgParticipants}</h6>
+        </div>
       </div>
     </div>
   )
